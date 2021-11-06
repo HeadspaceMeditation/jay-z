11a4c4ccb993be23e4bf561b97d4ef9875a8fc60
Josh Carver <jcarver989@gmail.com>
2021-11-05 22:30:59 -0700

    chore: Upgrade to  aws-sdk v3.x

--- src/main/key-providers/KmsKeyProvider.ts
+++ src/main/key-providers/KmsKeyProvider.ts
@@ -1,23 +1,28 @@
-import { KMS } from "aws-sdk"
+import {
+  DecryptCommand,
+  GenerateDataKeyCommand,
+  KMSClient
+} from "@aws-sdk/client-kms"
 import { crypto_kdf_KEYBYTES } from "libsodium-wrappers"
 import {
   DecryptDataKeyResult,
   GenerateDataKeyResult,
   KeyProvider
-} from "./DataKeyProvider"
+} from "./KeyProvider"
 
 /** A KeyProvider that uses an AWS KMS CMK to generate data keys */
 export class KmsKeyProvider implements KeyProvider {
-  constructor(private keyId: string, private kms: KMS = new KMS()) {}
+  constructor(
+    private keyId: string,
+    private kms: KMSClient = new KMSClient({})
+  ) {}
 
   async generateDataKey(): Promise<GenerateDataKeyResult> {
-    const result = await this.kms
-      .generateDataKey({
-        KeyId: this.keyId,
-        NumberOfBytes: crypto_kdf_KEYBYTES
-      })
-      .promise()
-
+    const command = new GenerateDataKeyCommand({
+      KeyId: this.keyId,
+      NumberOfBytes: crypto_kdf_KEYBYTES
+    })
+    const result = await this.kms.send(command)
     const plaintextKey = result.Plaintext as Uint8Array
     const encryptedKey = result.CiphertextBlob as Uint8Array
     return { plaintextKey, encryptedKey }
@@ -26,13 +31,11 @@ export class KmsKeyProvider implements KeyProvider {
   async decryptDataKey(
     encryptedDataKey: Uint8Array
   ): Promise<DecryptDataKeyResult> {
-    const result = await this.kms
-      .decrypt({
-        KeyId: this.keyId,
-        CiphertextBlob: encryptedDataKey
-      })
-      .promise()
-
+    const command = new DecryptCommand({
+      KeyId: this.keyId,
+      CiphertextBlob: encryptedDataKey
+    })
+    const result = await this.kms.send(command)
     return { plaintextKey: result.Plaintext as Uint8Array }
   }
 }
