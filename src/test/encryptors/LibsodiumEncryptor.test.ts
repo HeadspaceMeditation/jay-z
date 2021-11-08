63ea7da9bd3cafc0de6097559ef85abd389dae28
Josh Carver <jcarver989@gmail.com>
2021-11-07 21:02:20 -0800

    fix: Bug where LibsodiumEncryptor was reusing a nonce across multiple encryption operations

--- src/test/encryptors/LibsodiumEncryptor.test.ts
+++ src/test/encryptors/LibsodiumEncryptor.test.ts
@@ -1,7 +1,8 @@
 import stringify from "fast-json-stable-stringify"
-import { crypto_secretbox_easy, from_string } from "libsodium-wrappers"
-import { LibsodiumEncryptor } from "../../main/encryptors"
-import { FixedKeyProvider } from "../../main/key-providers"
+import { crypto_secretbox_easy, crypto_secretbox_NONCEBYTES, from_string, randombytes_buf } from "libsodium-wrappers"
+import { EncryptProps, EncryptResult, ItemWithEncryptedFields, LibsodiumEncryptor } from "../../main/encryptors"
+import { serialize } from "../../main/encryptors/serialization"
+import { FixedKeyProvider, KeyProvider } from "../../main/key-providers"
 import { aBankAccount, BankAccount } from "../util"
 
 describe("LibsodiumEncryptor", () => {
@@ -13,25 +14,36 @@ describe("LibsodiumEncryptor", () => {
     const { plaintextKey } = await keyProvider.generateDataKey()
     const { encryptedItem } = await encryptor.encrypt({
       item: account,
-      fieldsToEncrypt
+      fieldsToEncrypt: ["accountNumber", "balance", "routingNumber", "notes"]
     })
 
     expect(encryptedItem.pk).toEqual("account-123")
     expect(encryptedItem.sk).toEqual("Flava Flav")
 
+    const fields = {} as any
     fieldsToEncrypt.forEach((fieldName) => {
-      const expectedValue = crypto_secretbox_easy(
-        from_string(stringify(account[fieldName])),
-        encryptedItem.__jayz__metadata.nonce,
-        plaintextKey
-      )
-      expect(encryptedItem[fieldName]).toEqual(expectedValue)
+      fields[fieldName] = account[fieldName]
     })
+
+    const encryptedFields = crypto_secretbox_easy(
+      from_string(stringify(fields)),
+      encryptedItem.__jayz__metadata.nonce,
+      plaintextKey
+    )
+
+    expect((encryptedItem.__jayz__metadata as any).encryptedFields).toEqual(encryptedFields)
   })
 
   it("should decrypt an item", async () => {
     const { encryptor } = await setup()
     const { encryptedItem } = await encryptor.encrypt({ item: account, fieldsToEncrypt })
+    const { decryptedItem } = await encryptor.decrypt({ item: encryptedItem })
+    expect(decryptedItem).toEqual(account)
+  })
+
+  it("should decrypt a legacy item", async () => {
+    const { encryptor, keyProvider } = await setup()
+    const { encryptedItem } = await legacyEncrypt(keyProvider, { item: account, fieldsToEncrypt })
     const { decryptedItem } = await encryptor.decrypt({ item: encryptedItem })
     expect(decryptedItem).toEqual(account)
   })
@@ -84,4 +96,43 @@ async function setup(): Promise<{ keyProvider: FixedKeyProvider; encryptor: Libs
   const keyProvider = await FixedKeyProvider.forLibsodium()
   const encryptor = new LibsodiumEncryptor({ keyProvider })
   return { encryptor, keyProvider }
+}
+
+/** This is a copy/pasted "legacy" version of LibsodiumEncryptor's encrypt function, which had
+ *  a bug that reused nonces across multiple encrypt operations.
+ *
+ *  It is preserved here for testing to ensure we can decrypt items encrypted with this format
+ *  */
+async function legacyEncrypt<T, U extends keyof T>(
+  keyProvider: KeyProvider,
+  props: EncryptProps<T, U>
+): Promise<EncryptResult<T, U>> {
+  const { item, fieldsToEncrypt } = props
+  const key = await keyProvider.generateDataKey()
+  const nonce = randombytes_buf(crypto_secretbox_NONCEBYTES)
+
+  const encryptedFields: {
+    [K in U]: Uint8Array
+  } = {} as ItemWithEncryptedFields<T, U>
+
+  fieldsToEncrypt.forEach((fieldName) => {
+    const fieldValue = item[fieldName]
+    if (fieldValue !== undefined && fieldValue !== null) {
+      encryptedFields[fieldName] = crypto_secretbox_easy(serialize(fieldValue), nonce, key.plaintextKey)
+    }
+  })
+
+  const encryptedItem = {
+    ...item,
+    ...encryptedFields,
+    __jayz__metadata: {
+      encryptedDataKey: key.encryptedKey,
+      keyId: key.metadata?.keyId,
+      nonce,
+      version: "v0",
+      encryptedFieldNames: fieldsToEncrypt
+    }
+  }
+
+  return { encryptedItem }
 }
