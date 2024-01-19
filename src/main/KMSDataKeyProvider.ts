2f7498ef3a34272521faf998082045a23fa0f020
Kelsey Edelstein <kelseyedelstein@gmail.com>
2024-01-19 13:28:54 -0800

    Use new KMS client

--- src/main/KMSDataKeyProvider.ts
+++ src/main/KMSDataKeyProvider.ts
@@ -1,18 +1,17 @@
-import { KMS } from "aws-sdk"
+import { DecryptCommand, GenerateDataKeyCommand, KMSClient } from "@aws-sdk/client-kms"
 import { crypto_kdf_KEYBYTES } from "libsodium-wrappers"
 import { DataKey, DataKeyProvider } from "./DataKeyProvider"
 
 /** A KeyProvider that uses an AWS KMS CMK to generate data keys */
 export class KMSDataKeyProvider implements DataKeyProvider {
-  constructor(private keyId: string, private kms: KMS = new KMS()) {}
+  constructor(private keyId: string, private kms: KMSClient = new KMSClient({})) {}
 
   async generateDataKey(): Promise<DataKey> {
-    const result = await this.kms
-      .generateDataKey({
-        KeyId: this.keyId,
-        NumberOfBytes: crypto_kdf_KEYBYTES
-      })
-      .promise()
+    const command = new GenerateDataKeyCommand({
+      KeyId: this.keyId,
+      NumberOfBytes: crypto_kdf_KEYBYTES
+    })
+    const result = await this.kms.send(command)
 
     const dataKey = result.Plaintext as Uint8Array
     const encryptedDataKey = result.CiphertextBlob as Uint8Array
@@ -24,12 +23,11 @@ export class KMSDataKeyProvider implements DataKeyProvider {
   }
 
   async decryptDataKey(encryptedDataKey: Uint8Array): Promise<Uint8Array> {
-    const result = await this.kms
-      .decrypt({
-        KeyId: this.keyId,
-        CiphertextBlob: encryptedDataKey
-      })
-      .promise()
+    const command = new DecryptCommand({
+      KeyId: this.keyId,
+      CiphertextBlob: encryptedDataKey
+    })
+    const result = await this.kms.send(command)
 
     return result.Plaintext as Uint8Array
   }
